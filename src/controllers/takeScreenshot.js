bb14ac8590a0dde6a16012af1f56395098004164
Halit Sever <halitsever@protonmail.com>
2022-10-03 02:24:47 +0300

    typo fix for version, update main script in package.json, add type safety for fileName property

--- src/controllers/takeScreenshot.js
+++ src/controllers/takeScreenshot.js
@@ -1,6 +1,6 @@
-const httpGet = require('../usecases/http/get/httpGet');
-const fs = require('fs/promises');
-const buffer = require('buffer');
+const httpGet = require("../usecases/http/get/httpGet");
+const fs = require("fs/promises");
+const buffer = require("buffer");
 /**
  *
  * @param {string} pageUrl - Website url
@@ -9,8 +9,16 @@ const buffer = require('buffer');
  * @param {string} fileName - Saving by name
  * @returns {Buffer}
  */
-module.exports = async function takeScreenshot({ pageUrl, screenSize, saveFile, fileName }) {
-  const screenshot = await httpGet(`https://shotsnap.vercel.app/api/screenshot?page=${pageUrl}`);
-  if (saveFile) await fs.writeFile(fileName, Buffer.from(screenshot));
+module.exports = async function takeScreenshot({
+  pageUrl,
+  screenSize,
+  saveFile,
+  fileName,
+}) {
+  const screenshot = await httpGet(
+    `https://shotsnap.vercel.app/api/screenshot?page=${pageUrl}`
+  );
+  if (saveFile)
+    await fs.writeFile(fileName || `shotsnap.png`, Buffer.from(screenshot));
   else return Buffer.from(screenshot);
 };
